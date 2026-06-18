1c9d61e909cdbe7bc37013b9fa725eb5bfa4f67c
Nikoolaii <nikolailemerre@gmail.com>
2026-06-18 18:41:12 +0200

    je t'en supplie

--- .adonisjs/client/registry/schema.d.ts
+++ .adonisjs/client/registry/schema.d.ts
@@ -1,178 +1,246 @@
 /* eslint-disable prettier/prettier */
 /// <reference path="../manifest.d.ts" />
 
-import type { ExtractBody, ExtractErrorResponse, ExtractQuery, ExtractQueryForGet, ExtractResponse } from '@tuyau/core/types'
+import type {
+  ExtractBody,
+  ExtractErrorResponse,
+  ExtractQuery,
+  ExtractQueryForGet,
+  ExtractResponse,
+} from '@tuyau/core/types'
 import type { InferInput, SimpleError } from '@vinejs/vine/types'
 
 export type ParamValue = string | number | bigint | boolean
 
 export interface Registry {
   'session.create': {
-    methods: ["GET","HEAD"]
+    methods: ['GET', 'HEAD']
     pattern: '/login'
     types: {
       body: {}
       paramsTuple: []
       params: {}
       query: {}
-      response: ExtractResponse<Awaited<ReturnType<import('#controllers/session_controller').default['create']>>>
-      errorResponse: ExtractErrorResponse<Awaited<ReturnType<import('#controllers/session_controller').default['create']>>>
+      response: ExtractResponse<
+        Awaited<ReturnType<import('#controllers/session_controller').default['create']>>
+      >
+      errorResponse: ExtractErrorResponse<
+        Awaited<ReturnType<import('#controllers/session_controller').default['create']>>
+      >
     }
   }
   'session.store': {
-    methods: ["POST"]
+    methods: ['POST']
     pattern: '/login'
     types: {
-      body: ExtractBody<InferInput<(typeof import('#validators/auth').loginValidator)>>
+      body: ExtractBody<InferInput<typeof import('#validators/auth').loginValidator>>
       paramsTuple: []
       params: {}
-      query: ExtractQuery<InferInput<(typeof import('#validators/auth').loginValidator)>>
-      response: ExtractResponse<Awaited<ReturnType<import('#controllers/session_controller').default['store']>>>
-      errorResponse: ExtractErrorResponse<Awaited<ReturnType<import('#controllers/session_controller').default['store']>>> | { status: 422; response: { errors: SimpleError[] } }
+      query: ExtractQuery<InferInput<typeof import('#validators/auth').loginValidator>>
+      response: ExtractResponse<
+        Awaited<ReturnType<import('#controllers/session_controller').default['store']>>
+      >
+      errorResponse:
+        | ExtractErrorResponse<
+            Awaited<ReturnType<import('#controllers/session_controller').default['store']>>
+          >
+        | { status: 422; response: { errors: SimpleError[] } }
     }
   }
   'session.register': {
-    methods: ["GET","HEAD"]
+    methods: ['GET', 'HEAD']
     pattern: '/register'
     types: {
       body: {}
       paramsTuple: []
       params: {}
       query: {}
-      response: ExtractResponse<Awaited<ReturnType<import('#controllers/session_controller').default['registerCreate']>>>
-      errorResponse: ExtractErrorResponse<Awaited<ReturnType<import('#controllers/session_controller').default['registerCreate']>>>
+      response: ExtractResponse<
+        Awaited<ReturnType<import('#controllers/session_controller').default['registerCreate']>>
+      >
+      errorResponse: ExtractErrorResponse<
+        Awaited<ReturnType<import('#controllers/session_controller').default['registerCreate']>>
+      >
     }
   }
   'session.registerStore': {
-    methods: ["POST"]
+    methods: ['POST']
     pattern: '/register'
     types: {
-      body: ExtractBody<InferInput<(typeof import('#validators/auth').registerValidator)>>
+      body: ExtractBody<InferInput<typeof import('#validators/auth').registerValidator>>
       paramsTuple: []
       params: {}
-      query: ExtractQuery<InferInput<(typeof import('#validators/auth').registerValidator)>>
-      response: ExtractResponse<Awaited<ReturnType<import('#controllers/session_controller').default['registerStore']>>>
-      errorResponse: ExtractErrorResponse<Awaited<ReturnType<import('#controllers/session_controller').default['registerStore']>>> | { status: 422; response: { errors: SimpleError[] } }
+      query: ExtractQuery<InferInput<typeof import('#validators/auth').registerValidator>>
+      response: ExtractResponse<
+        Awaited<ReturnType<import('#controllers/session_controller').default['registerStore']>>
+      >
+      errorResponse:
+        | ExtractErrorResponse<
+            Awaited<ReturnType<import('#controllers/session_controller').default['registerStore']>>
+          >
+        | { status: 422; response: { errors: SimpleError[] } }
     }
   }
   'session.destroy': {
-    methods: ["DELETE"]
+    methods: ['DELETE']
     pattern: '/logout'
     types: {
       body: {}
       paramsTuple: []
       params: {}
       query: {}
-      response: ExtractResponse<Awaited<ReturnType<import('#controllers/session_controller').default['destroy']>>>
-      errorResponse: ExtractErrorResponse<Awaited<ReturnType<import('#controllers/session_controller').default['destroy']>>>
+      response: ExtractResponse<
+        Awaited<ReturnType<import('#controllers/session_controller').default['destroy']>>
+      >
+      errorResponse: ExtractErrorResponse<
+        Awaited<ReturnType<import('#controllers/session_controller').default['destroy']>>
+      >
     }
   }
   'profile.index': {
-    methods: ["GET","HEAD"]
+    methods: ['GET', 'HEAD']
     pattern: '/profil'
     types: {
       body: {}
       paramsTuple: []
       params: {}
       query: {}
-      response: ExtractResponse<Awaited<ReturnType<import('#controllers/profile_controller').default['index']>>>
-      errorResponse: ExtractErrorResponse<Awaited<ReturnType<import('#controllers/profile_controller').default['index']>>>
+      response: ExtractResponse<
+        Awaited<ReturnType<import('#controllers/profile_controller').default['index']>>
+      >
+      errorResponse: ExtractErrorResponse<
+        Awaited<ReturnType<import('#controllers/profile_controller').default['index']>>
+      >
     }
   }
   'api.packs.generateCode': {
-    methods: ["POST"]
+    methods: ['POST']
     pattern: '/api/packs/generate-code'
     types: {
       body: {}
       paramsTuple: []
       params: {}
       query: {}
-      response: ExtractResponse<Awaited<ReturnType<import('#controllers/api/packs_controller').default['generateCode']>>>
-      errorResponse: ExtractErrorResponse<Awaited<ReturnType<import('#controllers/api/packs_controller').default['generateCode']>>>
+      response: ExtractResponse<
+        Awaited<ReturnType<import('#controllers/api/packs_controller').default['generateCode']>>
+      >
+      errorResponse: ExtractErrorResponse<
+        Awaited<ReturnType<import('#controllers/api/packs_controller').default['generateCode']>>
+      >
     }
   }
   'home': {
-    methods: ["GET","HEAD"]
+    methods: ['GET', 'HEAD']
     pattern: '/'
     types: {
       body: {}
       paramsTuple: []
       params: {}
       query: {}
-      response: ExtractResponse<Awaited<ReturnType<import('#controllers/home_controller').default['drop']>>>
-      errorResponse: ExtractErrorResponse<Awaited<ReturnType<import('#controllers/home_controller').default['drop']>>>
+      response: ExtractResponse<
+        Awaited<ReturnType<import('#controllers/home_controller').default['drop']>>
+      >
+      errorResponse: ExtractErrorResponse<
+        Awaited<ReturnType<import('#controllers/home_controller').default['drop']>>
+      >
     }
   }
   'michelin.pourquoi': {
-    methods: ["GET","HEAD"]
+    methods: ['GET', 'HEAD']
     pattern: '/pourquoi-michelin'
     types: {
       body: {}
       paramsTuple: []
       params: {}
       query: {}
-      response: ExtractResponse<Awaited<ReturnType<import('#controllers/home_controller').default['pourquoi']>>>
-      errorResponse: ExtractErrorResponse<Awaited<ReturnType<import('#controllers/home_controller').default['pourquoi']>>>
+      response: ExtractResponse<
+        Awaited<ReturnType<import('#controllers/home_controller').default['pourquoi']>>
+      >
+      errorResponse: ExtractErrorResponse<
+        Awaited<ReturnType<import('#controllers/home_controller').default['pourquoi']>>
+      >
     }
   }
   'api.auth.login': {
-    methods: ["POST"]
+    methods: ['POST']
     pattern: '/api/auth/login'
     types: {
-      body: ExtractBody<InferInput<(typeof import('#validators/auth').loginValidator)>>
+      body: ExtractBody<InferInput<typeof import('#validators/auth').loginValidator>>
       paramsTuple: []
       params: {}
-      query: ExtractQuery<InferInput<(typeof import('#validators/auth').loginValidator)>>
-      response: ExtractResponse<Awaited<ReturnType<import('#controllers/api/auth_controller').default['store']>>>
-      errorResponse: ExtractErrorResponse<Awaited<ReturnType<import('#controllers/api/auth_controller').default['store']>>> | { status: 422; response: { errors: SimpleError[] } }
+      query: ExtractQuery<InferInput<typeof import('#validators/auth').loginValidator>>
+      response: ExtractResponse<
+        Awaited<ReturnType<import('#controllers/api/auth_controller').default['store']>>
+      >
+      errorResponse:
+        | ExtractErrorResponse<
+            Awaited<ReturnType<import('#controllers/api/auth_controller').default['store']>>
+          >
+        | { status: 422; response: { errors: SimpleError[] } }
     }
   }
   'api.auth.logout': {
-    methods: ["DELETE"]
+    methods: ['DELETE']
     pattern: '/api/auth/logout'
     types: {
       body: {}
       paramsTuple: []
       params: {}
       query: {}
-      response: ExtractResponse<Awaited<ReturnType<import('#controllers/api/auth_controller').default['destroy']>>>
-      errorResponse: ExtractErrorResponse<Awaited<ReturnType<import('#controllers/api/auth_controller').default['destroy']>>>
+      response: ExtractResponse<
+        Awaited<ReturnType<import('#controllers/api/auth_controller').default['destroy']>>
+      >
+      errorResponse: ExtractErrorResponse<
+        Awaited<ReturnType<import('#controllers/api/auth_controller').default['destroy']>>
+      >
     }
   }
   'api.drops.context': {
-    methods: ["GET","HEAD"]
+    methods: ['GET', 'HEAD']
     pattern: '/api/drops/:dropId/context'
     types: {
       body: {}
       paramsTuple: [ParamValue]
       params: { dropId: ParamValue }
       query: {}
-      response: ExtractResponse<Awaited<ReturnType<import('#controllers/api/drops_controller').default['context']>>>
-      errorResponse: ExtractErrorResponse<Awaited<ReturnType<import('#controllers/api/drops_controller').default['context']>>>
+      response: ExtractResponse<
+        Awaited<ReturnType<import('#controllers/api/drops_controller').default['context']>>
+      >
+      errorResponse: ExtractErrorResponse<
+        Awaited<ReturnType<import('#controllers/api/drops_controller').default['context']>>
+      >
     }
   }
   'api.drops.packs': {
-    methods: ["GET","HEAD"]
+    methods: ['GET', 'HEAD']
     pattern: '/api/drops/:dropId/packs'
     types: {
       body: {}
       paramsTuple: [ParamValue]
       params: { dropId: ParamValue }
       query: {}
-      response: ExtractResponse<Awaited<ReturnType<import('#controllers/api/drops_controller').default['packs']>>>
-      errorResponse: ExtractErrorResponse<Awaited<ReturnType<import('#controllers/api/drops_controller').default['packs']>>>
+      response: ExtractResponse<
+        Awaited<ReturnType<import('#controllers/api/drops_controller').default['packs']>>
+      >
+      errorResponse: ExtractErrorResponse<
+        Awaited<ReturnType<import('#controllers/api/drops_controller').default['packs']>>
+      >
     }
   }
   'api.products.reviews': {
-    methods: ["GET","HEAD"]
+    methods: ['GET', 'HEAD']
     pattern: '/api/products/:productId/reviews'
     types: {
       body: {}
       paramsTuple: [ParamValue]
       params: { productId: ParamValue }
       query: {}
-      response: ExtractResponse<Awaited<ReturnType<import('#controllers/api/products_controller').default['reviews']>>>
-      errorResponse: ExtractErrorResponse<Awaited<ReturnType<import('#controllers/api/products_controller').default['reviews']>>>
+      response: ExtractResponse<
+        Awaited<ReturnType<import('#controllers/api/products_controller').default['reviews']>>
+      >
+      errorResponse: ExtractErrorResponse<
+        Awaited<ReturnType<import('#controllers/api/products_controller').default['reviews']>>
+      >
     }
   }
 }
--- .adonisjs/client/registry/tree.d.ts
+++ .adonisjs/client/registry/tree.d.ts
@@ -2,34 +2,34 @@
 import type { routes } from './index.ts'
 
 export interface ApiDefinition {
-  home: typeof routes['home']
+  home: (typeof routes)['home']
   session: {
-    create: typeof routes['session.create']
-    store: typeof routes['session.store']
-    register: typeof routes['session.register']
-    registerStore: typeof routes['session.registerStore']
-    destroy: typeof routes['session.destroy']
+    create: (typeof routes)['session.create']
+    store: (typeof routes)['session.store']
+    register: (typeof routes)['session.register']
+    registerStore: (typeof routes)['session.registerStore']
+    destroy: (typeof routes)['session.destroy']
   }
   profile: {
-    index: typeof routes['profile.index']
+    index: (typeof routes)['profile.index']
   }
   michelin: {
-    pourquoi: typeof routes['michelin.pourquoi']
+    pourquoi: (typeof routes)['michelin.pourquoi']
   }
   api: {
     auth: {
-      login: typeof routes['api.auth.login']
-      logout: typeof routes['api.auth.logout']
+      login: (typeof routes)['api.auth.login']
+      logout: (typeof routes)['api.auth.logout']
     }
     drops: {
-      context: typeof routes['api.drops.context']
-      packs: typeof routes['api.drops.packs']
+      context: (typeof routes)['api.drops.context']
+      packs: (typeof routes)['api.drops.packs']
     }
     products: {
-      reviews: typeof routes['api.products.reviews']
+      reviews: (typeof routes)['api.products.reviews']
     }
     packs: {
-      generateCode: typeof routes['api.packs.generateCode']
+      generateCode: (typeof routes)['api.packs.generateCode']
     }
   }
 }
